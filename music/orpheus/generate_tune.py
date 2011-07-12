0bba4415f9019cfcfb67677ca18d58961c741cd6
Takahiro Kamatani <buhii314@gmail.com>
2011-07-12 21:22:30 +0900

    added print_js function

--- music/orpheus/generate_tune.py
+++ music/orpheus/generate_tune.py
@@ -1,65 +1,21 @@
-def melody2js(melody):
-    js = []
-    conte = melody2conte(melody)
+class TripletError(Exception): pass
 
-    tmp = []
-    for i in range(len(melody)):
-        t = melody[i]
-        if conte[i] == -1:
-            if not tmp:
-                tmp = [999] * 3
-	elif conte[i] == 1:
-            tmp = [t, t+1, t+2]
-	elif conte[i] == 2:
-            tmp = [t-1, t, t+1]
-	elif conte[i] == 3:
-            tmp = [t-2, t-1, t]
-
-        if len(tmp) == 3:
-            tmp.append(999)
-	js.append(tmp)
-    return js
-
-
-def melody2conte(melody):
-    m_min = min(melody)
-    m_max = max(map(lambda x: x if x != 999 else m_min, melody))
-
-    d = m_max - m_min + 1
-
-    ret = []
-    for m in melody:
-        if m != 999:
-            ret.append(int((m - m_min) * 3.0 / d) + 1)
-	else:
-            ret.append(-1)
-    return ret
-
-
-N = 999
-
-
-def quantize_3(l):
-    def filter_nan(m):
-        ret = filter(lambda c: c != N, m)
-        if ret:
-            return ret[0]
-        else:
-            return N
-
-    tri = zip(l[0::3], l[1::3], l[2::3])
-    return map(filter_nan, tri)
-
-
-orpheus = [
+Z = 999
+melody = [
     #1   2   3   4   5   6   7   8
     57, 64, 64, 66, 64, 62, 62, 66,
+    67, 71, 74, 71, 71, 69, 69,  Z,
+
+    71, 61, 61, 71, 69, 62, 62, 66,
+    66, 64, 66, 64, 66, 64, 66, 64,
+
+    57, 64, 64, 66, 64, 62, 62, 66,
+    67, 71, 74, 71, 71, 69, 69,  Z,
     
+    71, 61, 61, 71, 69, 62, 62, 66,
+    66, 64, 66, 64, 64, 62, 62,  Z,
 ]
 
-
-def print_map(l):
-    print "tune = %s" % repr(l)
 
 def print_js(l):
     count = 0
@@ -68,6 +24,47 @@ def print_js(l):
         count = (count + 1) % 8
         if not count: print ""
 
-print_map(melody2conte(mito))
-print_js(melody2js(quantize_3(mito)))
+
+def div_bar(l, div=4):
+    tmp = []
+    for i in range(len(l) / div):
+        tmp.append(l[i * div: (i + 1) * div])
+    return tmp
+
+
+def gen_triplet(l):
+    sorted_list = sorted(list(set(l)))
+    if Z in sorted_list:
+        sorted_list.remove(Z)
+
+    if len(sorted_list) > 3:
+        raise TripletError("over 3!", l)
+    elif len(sorted_list) == 2:
+        center = (min(sorted_list) + max(sorted_list)) / 2
+        sorted_list.insert(1, center)
+    elif len(sorted_list) == 1:
+        p = sorted_list[0]
+        sorted_list.insert(0, p - 1)
+        sorted_list.append(p + 1)
+    return sorted_list
+
+
+def print_js(triplets):
+    for tpl in triplets:
+        tmp = tpl[:]
+        tmp.append(Z)
+        for i in range(4):
+            print "\t%s," % tmp
+
+
+def print_map(melody, l):
+    print "tune = %s" % repr(l)
+
+
+
+if __name__ == '__main__':
+    triplets = map(gen_triplet, div_bar(melody))
+    print_js(triplets)
+    print_map(melody, triplets)
+
 
